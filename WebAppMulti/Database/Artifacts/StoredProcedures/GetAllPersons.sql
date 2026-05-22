d5f1941376efb6302d2fa4d1902b0e11d9bc51d9
Frank Mastronardi <mastronardif@gmail.com>
2026-05-22 12:29:36 -0400

    Clenaup gett schema artifacts  from task/export now.

--- WebAppMulti/Database/Artifacts/StoredProcedures/GetAllPersons.sql
+++
@@ -1,8 +0,0 @@
-﻿-- drop PROCEDURE GetAllPersons
-CREATE PROCEDURE GetAllPersons
-AS
-BEGIN
-    SET NOCOUNT ON;
-    SELECT [EmployeeKey], [FirstName] as EmployeeName FROM  [dbo].[DimEmployee];
-END
--- use [AdventureWorksDW] exec GetAllPersons
